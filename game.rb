f727ac0b044ef7d16ace5ff2893f7249fac3c70a
ktamulonis <kurttamulonis@gmail.com>
2019-02-12 09:10:13 -0500

    added game spec

--- game.rb
+++ game.rb
@@ -1,7 +1,24 @@
-class Game
+require 'gosu'
+
+class Game < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Game"
+  end
   
+  def update
+    # ...
+  end
+  
+  def draw
+    # ...
+  end
+
   def name
     'Game'
   end
 
 end
+
+# Game.new.show
+
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper.rb'
 
 describe "Game" do 
+
+  describe ".new" do 
+    it "returns a Game object" do 
+      expect(Game.new).to be_an_instance_of Game
+    end
+  end
+
   describe "name" do
     it "returns a default name of Game" do 
       expect(Game.new.name).to eq 'Game'
     end
   end
+
+  describe ".draw" do
+  end
+
+  describe ".update" do 
+  end
+
 end
